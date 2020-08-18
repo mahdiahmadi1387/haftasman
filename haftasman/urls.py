63325d5d1a1e53ccce466a5836c3b17ef4fb231d
Mahdi Ahmadi <mahdi.ahmadi.py@gmail.com>
2020-08-18 12:20:30 +0430

    django first commitgit status!

---
+++ haftasman/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home),
+    path('haftasmanapp/',include('haftasmanapp.urls'))
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+
---
+++ haftasmanapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HaftasmanappConfig(AppConfig):
+    name = 'haftasmanapp'
